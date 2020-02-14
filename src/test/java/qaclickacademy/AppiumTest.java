97de16fc7cb04b5bea804d4db263d9612de2f932
Priyanka211092 <priyanka21sharma10@gmail.com>
2020-02-15 02:23:23 +0530

    Adding my project to repository

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void SecondTest1()
+	{
+		System.out.println("dummy test 2 method 1");
+	}
+	
+	@Test
+	public void SecondTest2()
+	{
+		System.out.println("dummy test 2 method 2");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	public void Dummymethod1()
+	{
+		System.out.println("dummy test 1 method 1");
+	}
+	
+	@Test
+	public void Dummymethod2()
+	{
+		System.out.println("dummy test 1 method 2");
+	}
+
+}
+
+
